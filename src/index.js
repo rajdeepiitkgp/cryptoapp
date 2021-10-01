f8aff72a044fa1129a8ff7dc24947dedbd742857
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2021-10-01 18:35:34 +0530

    Hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "antd/dist/antd.css";
 import store from "./App/store";
